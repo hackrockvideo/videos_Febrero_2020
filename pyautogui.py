196a3f8d55c8af7e8ef0bc7e78d4223ae3cb9b27
hackrockvideo <giunicit@gmail.com>
2020-02-29 05:11:09 +0000

    Update pyautogui.py

--- pyautogui.py
+++ pyautogui.py
@@ -1,1 +1,11 @@
+#!/usr/bin/env python
+# _*_ coding: utf8 _*_
 
+import pyautogui
+
+print ("Script para nmap\n")
+ip = raw_input ("rhost: ")
+
+pyautogui.typewrite("nmap -sC -sV ")
+pyautogui.typewrite(ip)
+pyautogui.typewrite(" -o result.txt")
